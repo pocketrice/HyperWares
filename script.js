5438d44012e8ec8e94f51e1cf6470aa672c7bbb7
PocketRice <79682953+PocketRice@users.noreply.github.com>
2022-04-29 17:36:16 -0400

    Logo Progress
    Slight tweaks to the logo; all set to wrap up and submit. Excellent!

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ window.addEventListener('load', function() {
 	base.addColorStop(0, "#FFFFFF"); // Color fix?
 	base.addColorStop(0.3, "#C2D6D7");
 	base.addColorStop(1, "#3E4D4F");*/
-	var base = "#FFFFFF"
+	var base = "#FFFFFF";
+
+	var border = ctx.createRadialGradient(182.5, 90.5, 10, 300, 200, 360);
+
+	border.addColorStop(0, "#51677D");
+	border.addColorStop(1, "#8CB1E0")
 
 	ctx.beginPath();
-	ctx.fillStyle = "#9DBBD8";
+	ctx.fillStyle = border;
 	ctx.arc(162.5, 137.5, 112.5, 0, Math.PI*2, true)
 	ctx.fill();
 	ctx.closePath();
@@ -45,20 +50,29 @@ window.addEventListener('load', function() {
 	ctx.fill();
 	ctx.closePath();
 
-	ctx.beginPath();
-	ctx.fillStyle = "#2585C5";
-	ctx.arc(162.5, 137.5, 102.5, 0, Math.PI*3.5, true);
-	ctx.fill();
-	ctx.closePath();
 
-	ctx.beginPath(); // TODO: make it curve inwards (bezier curve??)
-	ctx.fillStyle = "#FFFFFF";
+	ctx.beginPath(); 
+	ctx.fillStyle = "#CCE8F6";
 	ctx.ellipse(165.5, 128.5, 80, 90, 0, Math.PI*2, 0, true);
 	ctx.fill();
 	ctx.closePath();
 
+	ctx.fillStyle = "#383D71";
+	ctx.font = '70px "Nugelo Serif"';
+	ctx.fillText('h', 120, 120);
+	ctx.fillText('w', 170, 180);
+
+/*
+		ctx.beginPath();
+	ctx.fillStyle = "#2585C5";
+	ctx.arc(162.5, 177.5, 112.5, 0, Math.PI*2, true);
+	ctx.fill();
+	ctx.closePath();*/
+
+/*
+	ctx.beginPath();
 	ctx.fillStyle = "#444FAD";
-	ctx.font = '70px "Cascadia Code Italic"';
-	ctx.fillText('h', 120, 100);
-	ctx.fillText('w', 212.5, 150)
+	ctx.bezierCurveTo(10, 10, 30, 30, 20, 20);
+	ctx.fill();
+	ctx.closePath();*/
 })
