b5ef389ebccb4bfddcaf8fee27acfd453065cc94
PocketRice <79682953+PocketRice@users.noreply.github.com>
2022-05-17 18:55:07 -0400

    Form Validation Checks
    Fully implemented responsive validation checks. Included tinycolor2 dependency to check the brightness of the color from ColorPicker. Neato!

--- script.js
+++ script.js
@@ -3,13 +3,74 @@ window.addEventListener('load', function() {
 	var navCount = 0;
 	var colorPicker = document.getElementById("color-picker");
 	var colorPickerDisplay = document.getElementById("color-picker-display");
-
+	var invalidMessage = ["Must be only alphabetic, dashes, or apostrophes", "Must be a valid email address", "This date is unavailable.", "This color is unavailable."]
+	var validMessage = ["Valid!", "Valid!", "Date available!", "Color available!"]
 
 	colorPicker.addEventListener("input", function() {
 		colorPickerDisplay.innerText = colorPicker.value;
 	});
 
-	
+
+	var validationFields = document.getElementsByClassName("inputNeedsValidation");
+	var validationLabel = document.getElementsByClassName("form-validator");
+
+	for (let i = 0; i < validationFields.length; i++)
+	{
+		validationFields[i].addEventListener('input', function() {
+			validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>cached</span>&nbsp;Checking validity...";
+			validationLabel[i].setAttribute("class", "form-validator");
+			validationLabel[i].classList.add('form-loading');
+
+			setTimeout(function() {
+				validationLabel[i].setAttribute("class", "form-validator");
+
+				if (validationFields[i].value.length == 0)
+				{
+					validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>cached</span>&nbsp;Checking validity...";
+					validationLabel[i].classList.add('form-loading');
+				}
+
+				validator: if (validationFields[i].checkValidity() == true)
+				{
+					console.assert(tinycolor(colorPicker.value).isValid(), "Invalid tinycolor!" + colorPicker.value);
+
+					if (i == 3)
+					{
+						if ((tinycolor(colorPicker.value).getBrightness() > 50) && (tinycolor(colorPicker.value).getBrightness() < 230))
+						{
+							validationLabel[i].classList.add('form-valid');
+							validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>done</span>&nbsp;" + validMessage[i];
+						}
+						else
+						{
+							validationLabel[i].classList.add('form-fail');
+							validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>error</span>&nbsp;" + invalidMessage[i];
+						}
+						break validator;
+					}
+
+						validationLabel[i].classList.add('form-valid');
+						validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>done</span>&nbsp;" + validMessage[i];
+				}
+				else
+				{
+					validationLabel[i].classList.add('form-fail');
+					validationLabel[i].innerHTML = "<span class='material-symbols-outlined'>error</span>&nbsp;" + invalidMessage[i];
+				}
+		}, 1500)
+
+				if (validationFields[i].value.length == 0) // If input is empty
+				{
+					validationLabel[i].style.opacity = 0;
+				}
+				else
+				{
+					validationLabel[i].style.opacity = 1;
+				}
+
+			})
+	}
+
 
 	function submit() {
 		alert("Your order has been successfully queued. Thank you!");
@@ -120,6 +181,6 @@ window.addEventListener('load', function() {
 	ctx.fill();
 	ctx.closePath();*/
 
-		colorPickerDisplay.innerText = colorPicker.value;
+	colorPickerDisplay.innerText = colorPicker.value;
 	console.log("Script loaded. (" + performance.now() + "ms)");
 })
